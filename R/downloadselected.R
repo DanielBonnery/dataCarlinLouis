0ae56dea2a38cdfefe4b8d18112adf501ea80343
daniel <daniel.bonnery@gmail.com>
2018-10-05 09:56:05 -0400

    a

--- R/downloadselected.R
+++ R/downloadselected.R
@@ -2,24 +2,24 @@
 #'
 #'@param ... arguments to be passed to read.csv
 #'@examples
-#'aspirin<-
+#'aspirin<-downloadDataCarlinLouisGeneric(url="https://www.counterpointstat.com/uploads/1/1/9/3/119383887/aspirin_data.txt",
+#'variablenames=c("patient","X","Y"),
+#'sep = "\t",header=FALSE)
 downloadDataCarlinLouisGeneric<-function(url,variablenames=NULL,...){  
   temp_dir<-tempdir()
   filename<-basename(url)
   destfile<-file.path(temp_dir,filename)
   download.file(url=url,destfile=destfile)
-  toto<-read.csv(destfile)
-  if(!is.null(variablenames)){names(toto)<-variable.names)
+  toto<-read.csv(destfile,...)
+  if(!is.null(variablenames)){names(toto)<-variablenames}
+  toto
   }
 
 
 
 downloadDataCarlinLouis_aspirin<-function(){  
-  temp_dir<-tempdir()
-  url="https://www.counterpointstat.com/uploads/1/1/9/3/119383887/aspirin_data.txt"
-  filename<-basename(url)
-  destfile<-file.path(temp_dir,filename)
-  download.file(url=url,destfile=destfile)
-  aspirin<-read.csv(destfile,sep = "\t",header=FALSE)
-  names(aspirin)<-c("patient","X","Y")
-  aspirin}
+  downloadDataCarlinLouisGeneric(url="https://www.counterpointstat.com/uploads/1/1/9/3/119383887/aspirin_data.txt",
+                                 variablenames=c("patient","X","Y"),
+                                 sep = "\t",header=FALSE)}
+
+
