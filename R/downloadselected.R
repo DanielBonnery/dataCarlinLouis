d4128dccc5bcbffce20dc1aea5e3ad5b83829075
daniel <daniel.bonnery@gmail.com>
2018-10-05 10:04:51 -0400

    a

--- R/downloadselected.R
+++ R/downloadselected.R
@@ -1,24 +1,22 @@
-
 #'
 #'@param ... arguments to be passed to read.csv
 #'@examples
 #'aspirin<-downloadDataCarlinLouisGeneric(url="https://www.counterpointstat.com/uploads/1/1/9/3/119383887/aspirin_data.txt",
 #'variablenames=c("patient","X","Y"),
 #'sep = "\t",header=FALSE)
-downloadDataCarlinLouisGeneric<-function(url,variablenames=NULL,...){  
+ddCLGeneric<-function(url,variablenames=NULL,...){  
   temp_dir<-tempdir()
   filename<-basename(url)
   destfile<-file.path(temp_dir,filename)
   download.file(url=url,destfile=destfile)
   toto<-read.csv(destfile,...)
   if(!is.null(variablenames)){names(toto)<-variablenames}
-  toto
-  }
+  toto}
 
 
 
-downloadDataCarlinLouis_aspirin<-function(){  
-  downloadDataCarlinLouisGeneric(url="https://www.counterpointstat.com/uploads/1/1/9/3/119383887/aspirin_data.txt",
+ddCL_aspirin<-function(){  
+  ddCLGeneric(url="https://www.counterpointstat.com/uploads/1/1/9/3/119383887/aspirin_data.txt",
                                  variablenames=c("patient","X","Y"),
                                  sep = "\t",header=FALSE)}
 
