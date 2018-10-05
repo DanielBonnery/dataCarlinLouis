f7aecafc11fea5d46cd6dd122e38113e480d43e0
daniel <daniel.bonnery@gmail.com>
2018-10-05 13:49:32 -0400

    a

--- R/downloadselected.R
+++ R/downloadselected.R
@@ -4,12 +4,12 @@
 #'aspirin<-downloadDataCarlinLouisGeneric(url="https://www.counterpointstat.com/uploads/1/1/9/3/119383887/aspirin_data.txt",
 #'variablenames=c("patient","X","Y"),
 #'sep = "\t",header=FALSE)
-ddCLGeneric<-function(url,variablenames=NULL,...){  
-  temp_dir<-tempdir()
-  filename<-basename(url)
-  destfile<-file.path(temp_dir,filename)
-  download.file(url=url,destfile=destfile)
-  toto<-read.csv(destfile,...)
+ddCLGeneric<-function(url,read.function=read.csv,variablenames=NULL,...){  
+  #temp_dir<-tempdir()
+  #filename<-basename(url)
+  #destfile<-file.path(temp_dir,filename)
+  #download.file(url=url,destfile=destfile)
+  toto<-read.function(url,...)
   if(!is.null(variablenames)){names(toto)<-variablenames}
   toto}
 
@@ -24,10 +24,16 @@ ddCL_aspirin<-function(){
 
 #'
 #'@examples
-#'aspirin<-ddCL_aspirin()
+#'land<-ddCL_land()
 ddCL_land<-function(){  
   land<-ddCLGeneric(url="https://www.counterpointstat.com/uploads/1/1/9/3/119383887/land_data.txt",
               sep = " ",header=TRUE)
   cbind(data.frame(property=1:nrow(land)),land)
   }
-
+#'
+#'@examples
+#'puro<-ddCL_puro()
+ddCL_puro<-function(){  
+  puro<-read.fwf("https://www.counterpointstat.com/uploads/1/1/9/3/119383887/puro_data.txt",widths=c(2,13,20))
+  names(puro)<-c("Case", "x","y")
+  puro}
